96e927ddd5dc5039ea27d3b115a54b7d863f9894
ien <ien@amsostudios.com>
2020-03-29 19:44:03 +0200

    avoid returning values in idl

--- bin/xsteg2_wasm/src/wasm_api.cpp
+++ bin/xsteg2_wasm/src/wasm_api.cpp
@@ -14,4 +14,21 @@ struct ex_helper
     }
 };
 
+class steganographer : private xsteg::steganographer
+{
+    using xs = xsteg::steganographer;
+
+    steganographer(const ien::image& img) : xs(img) { }
+
+    void add_threshold(const xsteg::threshold& thres, bool apply) { xs::add_threshold(thres, apply); }
+
+    void clear_thresholds() { xs::clear_thresholds(); }
+
+    image* encode(const uint8_t* data, size_t len, const xsteg::encoding_options& eopts)
+    {
+        ien::image img = xs::encode(data, len, eopts);
+        return new ien::image(std::move(img));
+    }
+};
+
 #include "wasm_api.glue.cpp"
